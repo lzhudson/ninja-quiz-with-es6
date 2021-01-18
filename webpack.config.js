c74dc7c3511127d1b67d0e38d91cce0e8a6fe75a
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-18 14:36:46 -0300

    chore: configure loader babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,17 @@ module.exports = {
     compress: true
   },
   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
