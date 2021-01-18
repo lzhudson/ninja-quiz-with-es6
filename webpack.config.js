4944f3c24625e3d7e6321630de39e9004c9e14c2
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-18 14:39:34 -0300

    fix: update path to output file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   entry: './app.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   devServer: {
